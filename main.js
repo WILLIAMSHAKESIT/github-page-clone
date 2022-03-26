c4db589e5c4e51432d03f7f69b005e279a9cc87a
unknown <williamonacle@gmail.com>
2022-03-26 23:59:34 +0800

    updated

---
+++ main.js
@@ -0,0 +1,76 @@
+let classList = [
+    {
+        querySelector:'.in-main-secondary',
+        animationClass:'scale-up-bottom',
+        parentClass:'.main-secondary-container'
+    },
+    {
+        querySelector:'.second-sec-li-img',
+        animationClass:'scale-up-bottom',
+        parentClass:'.second-section-fourth-li'
+    },
+    {
+        querySelector:'.second-sec-title',
+        animationClass:'fade-to-right',
+        parentClass:'.second-sec-li-seven-cont'
+    },
+    {
+        querySelector:'.li-h3',
+        animationClass:'fade-to-right',
+        parentClass:'.li-three'
+    },
+    {
+        querySelector:'.li-three-img',
+        animationClass:'fade-to-left',
+        parentClass:'.li-three'
+    },
+    {
+        querySelector:'.li-h3',
+        animationClass:'fade-to-right',
+        parentClass:'.li-four'
+    },
+    {
+        querySelector:'.li-four-img',
+        animationClass:'fade-to-left',
+        parentClass:'.li-four'
+    },
+    {
+        querySelector:'.li-h3',
+        animationClass:'fade-to-right',
+        parentClass:'.li-five'
+    },
+    {
+        querySelector:'.li-five-img',
+        animationClass:'fade-to-left',
+        parentClass:'.li-five'
+    },
+    {
+        querySelector:'.iphone-home',
+        animationClass:'normal-fade',
+        parentClass:'.li-seven-main-cont'
+    },
+    {
+        querySelector:'.section-three-bg-type',
+        animationClass:'fade-to-left',
+        parentClass:'.li-seven-main-cont'
+    }
+]
+
+classList.forEach(item =>{
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+        const square = entry.target.querySelector(item.querySelector);
+    
+        if (entry.isIntersecting) {
+            square.classList.add(item.animationClass);
+            return; // if we added the class, exit the function
+        }
+            // We're not intersecting, so remove the class!
+            square.classList.remove(item.animationClass);
+        });
+    });
+    observer.observe(document.querySelector(item.parentClass));
+})
+
+
+console.log(document.body.clientHeight)
